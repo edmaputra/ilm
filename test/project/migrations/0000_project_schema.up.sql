438418298650f6a6fc6c453d1b36109492fd8c15
edmaputra <bangun.edma.saputra@gmail.comls>
2025-09-22 21:13:42 +0700

    start from scratch (again)

--- test/project/migrations/0000_project_schema.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS project (
-    id             VARCHAR(255) PRIMARY KEY,
-    name           VARCHAR(255) NOT NULL,
-    description    TEXT,
-    flow_stages_id VARCHAR(255),
-    created_at     INTEGER,
-    created_by     VARCHAR(255),
-    updated_at     INTEGER
-);
